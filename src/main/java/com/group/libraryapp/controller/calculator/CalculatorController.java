ed1b276139dbda6110ced49d0f5226b4d08f78ac
yujin <youjin010402@naver.com>
2024-03-07 15:22:26 +0900

    feat: POST /multiply

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,14 +1,23 @@
 package com.group.libraryapp.controller.calculator;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 
 @RestController
 public class CalculatorController {
-	@GetMapping("/add")
+	@GetMapping("/add") // GET /add
 	public int addTwoNumbers(CalculatorAddRequest request) {
 		return request.getNumber1() + request.getNumber2();
 	}
+
+	@PostMapping("/multiply") // POST /multiply * POST 인 경우 @RequestBody 필수! Body 넘겨 주기
+	public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+		return request.getNumber1() * request.getNumber2();
+	}
+
 }
--- src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorAddRequest.java
+++ src/main/java/com/group/libraryapp/dto/calculator/request/CalculatorAddRequest.java
@@ -4,7 +4,7 @@ public class CalculatorAddRequest {
 	private final int number1;
 	private final int number2;
 
-	public CalculatorAddRequest(int number1, int number2) {
+	public CalculatorAddRequest(int number1, int number2){
 		this.number1 = number1;
 		this.number2 = number2;
 	}
