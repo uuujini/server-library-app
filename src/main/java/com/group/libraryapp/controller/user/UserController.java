37df796c493a2869f8653f947367f1cff8d7b078
yujin <youjin010402@naver.com>
2024-03-27 15:22:48 +0900

    feat: GET /user sql문 기준

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,12 +1,14 @@
 package com.group.libraryapp.controller.user;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-import com.group.libraryapp.domain.User;
 import com.group.libraryapp.dto.request.user.UserCreateRequest;
 import com.group.libraryapp.dto.response.UserResponse;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,20 +17,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-	private final List<User> users = new ArrayList<>();
+	// private final List<User> users = new ArrayList<>();
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public UserController(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@PostMapping("/user") // POST /user
 	public void saveUser(@RequestBody UserCreateRequest request) {
-		users.add(new User(request.getName(), request.getAge()));
+		// users.add(new User(request.getName(), request.getAge()));
+		String sql = "INSERT INTO user(name, age) VALUES (?, ?)";
+		jdbcTemplate.update(sql, request.getName(), request.getAge());
 	}
 
 	@GetMapping("/user") // GET /user
 	public List<UserResponse> getUsers() {
-		List<UserResponse> responses = new ArrayList<>();
-		for (int i = 0; i < users.size(); i++) {
-			responses.add(new UserResponse(i + 1, users.get(i).getName(), users.get(i).getAge()));
-		}
-		return responses;
+		// 	List<UserResponse> responses = new ArrayList<>();
+		// 	for (int i = 0; i < users.size(); i++) {
+		// 		responses.add(new UserResponse(i + 1, users.get(i).getName(), users.get(i).getAge()));
+		// 	}
+		// 	return responses;
+		String sql = "SELECT * FROM user";
+		return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+			@Override
+			public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+				long id = rs.getLong("id");
+				String name = rs.getString(("name"));
+				int age = rs.getInt("age");
+				return new UserResponse(id, name, age);
+			}
+		});
 	}
-
 }
