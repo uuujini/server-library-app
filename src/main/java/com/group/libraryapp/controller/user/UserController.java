74eab6bd7b4e013501cb0fa78d23512fd551a567
yujin <youjin010402@naver.com>
2024-03-27 15:35:14 +0900

    feat: PUT /user sql Update문

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -4,14 +4,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.group.libraryapp.dto.request.UserUpdateRequest;
 import com.group.libraryapp.dto.request.user.UserCreateRequest;
 import com.group.libraryapp.dto.response.UserResponse;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -50,4 +54,11 @@ public class UserController {
 			}
 		});
 	}
+
+	@PutMapping("/user")
+	public void updateUser(@RequestBody UserUpdateRequest request) {
+		String sql = "UPDATE user SET name = ? WHERE id = ?";
+		jdbcTemplate.update(sql, request.getName(), request.getId());
+	}
+
 }
