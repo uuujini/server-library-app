65321c7b2fd5b25952bdc7d2fdf440b819f889a9
yujin <youjin010402@naver.com>
2024-04-05 18:53:06 +0900

    refactor: Configuration + Bean 사용하는 경우

--- src/main/java/com/group/libraryapp/repository/user/UserRepository.java
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -3,13 +3,16 @@ package com.group.libraryapp.repository.user;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
 
 import com.group.libraryapp.dto.response.UserResponse;
 
-@Repository
 public class UserRepository {
 
+	private final JdbcTemplate jdbcTemplate;
+
+	public UserRepository(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 	public boolean isUserNotExist(long id) {
 		String readSql = "SELECT * FROM user WHERE id = ?";
 		return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, id).isEmpty();
